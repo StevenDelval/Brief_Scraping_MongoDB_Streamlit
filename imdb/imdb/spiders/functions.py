9630731001624bf91643eacd68eb701b6a27dd0b
StevenDelval <asteven59220@free.fr>
2022-12-16 14:29:27 +0100

    nom des variables

--- imdb/imdb/spiders/functions.py
+++ imdb/imdb/spiders/functions.py
@@ -1,9 +1,9 @@
 import re
 def convertion_duree(str):
     
-    split_cell = re.split("\s", str)
+    split_liste = re.split("\s", str)
     duree = 0
-    for elt in split_cell:
+    for elt in split_liste:
         if "h" in elt:
             duree = int(re.split("h", elt)[0]) *60 +duree
         elif "m" in elt:
